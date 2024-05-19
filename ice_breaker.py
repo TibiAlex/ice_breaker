9723f02bc5dcb388f37a58315e0d745220e4871a
TibiAlex <tibi.alex@yahoo.com>
2024-05-19 12:42:05 +0300

    Finish linkedin request

---
+++ ice_breaker.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+
+from third_parties.linkedin import scrape_linkedin_profile
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Hello World")
+
+    summary_template = """
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    linkedIn_data = scrape_linkedin_profile(linkedin_profile_url="https://linkedin.com/in/eden-marco/")
+
+    res = chain.invoke(input={"information": linkedIn_data})
+
+    print(res)
